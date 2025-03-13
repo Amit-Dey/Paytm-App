99a2667866012210cce574676621d4eade83f459
Amit-Dey <amitdey9020@gmail.com>
2025-03-13 08:35:09 +0600

    initial commit

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+//@ts-ignore
+import { useBalance } from "@repo/store/balance";
+
+export default function() {
+  const balance = useBalance();
+  return <div>
+    hi there {balance}
+  </div>
+}
